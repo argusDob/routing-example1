88efe63bc9f50781b71567fbb5eb1bc5ac8c796b
ioannis <ioannis.dimitriou@emark.com>
2023-03-31 14:01:32 +0200

    refactor-nav-list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link v-for="link in links" :key="link.path" :to="link.path">{{
+        link.label + ""
+      }} | </router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+import navLinksList from './router/navLinks';
+
+export default {
+  data() {
+    return {
+      links: navLinksList,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/router/navLinks.js
@@ -0,0 +1,6 @@
+export default [
+  { path: '/', label: 'Home' },
+  { path: '/login', label: 'Login' },
+  { path: '/dashboard', label: 'Dashboard' },
+  { path: '/about', label: 'About' },
+];
