fc8c2a9b8569768273da22f6bc26cb7c2491e7e1
ioannis <ioannis.dimitriou@emark.com>
2023-04-03 22:43:58 +0200

    home-view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeInput/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomeInput from '@/components/Home/HomeInput.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    HomeInput,
   },
 };
 </script>
