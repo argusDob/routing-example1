14460b8bc3141a59df9b72b5333c75d8fa2080b7
ioannis <ioannis.dimitriou@emark.com>
2023-04-03 22:57:38 +0200

    primary-button-implementation

---
+++ src/components/PrimaryButton/PrimaryButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :disabled=isDisabled class="primary-button">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./PrimaryButton.scss"></style>
