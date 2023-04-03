16547fb48e4002eda0a0b57b6acd38eb75e65b9a
ioannis <ioannis.dimitriou@emark.com>
2023-04-03 22:45:02 +0200

    login-view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login-form-view">
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm/LoginForm.vue';
+
+export default {
+  name: 'LoginView',
+  components: {
+    LoginForm,
+  },
+};
+</script>
