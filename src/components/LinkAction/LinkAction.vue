71b38699968fdd9856f85e9e30bf957c212dcb79
ioannis <ioannis.dimitriou@emark.com>
2023-04-03 22:43:06 +0200

    introduce-link-action-component

---
+++ src/components/LinkAction/LinkAction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav-bar">
+    <router-link :to="path">{{ label }} </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
