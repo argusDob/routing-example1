10b45d222d46128305914026d3b69adb8a8562a2
ioannis <ioannis.dimitriou@emark.com>
2023-04-03 23:23:09 +0200

    cleanup

--- src/components/Home/HomeInput.vue
+++ src/components/Home/HomeInput.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="home-input">
-      <h1>Home</h1>
-      <LinkAction v-if="isAuthenticated" label="Go to Dashboard" path="/dashboard"  />
-    </div>
-  </template>
+  <div class="home-input">
+    <h1>Home</h1>
+    <LinkAction v-if="isAuthenticated" label="Go to Dashboard" path="/dashboard" />
+  </div>
+</template>
 
 <script>
-
 import { mapState } from 'vuex';
 import LinkAction from '../LinkAction/LinkAction.vue';
 
